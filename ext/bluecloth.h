c69957ceb6b85dc3a37c41b494a1854691efd43d
foretribe <foretribe@gmail.com>
2015-11-13 16:57:23 +1100

    commit for windows 7

--- ext/bluecloth.h
+++ ext/bluecloth.h
@@ -6,20 +6,21 @@
 
 #ifndef BLUECLOTH_H
 #define BLUECLOTH_H
+#include "ruby.h"
+#if defined(HAVE_RUBY_ENCODING_H) && HAVE_RUBY_ENCODING_H
+#	define M17N_SUPPORTED
+#	include "ruby/encoding.h"
+#endif
 
 #include "config.h"
 #include "assert.h"
 
 #include "mkdio.h"
-#include "ruby.h"
+//#include "ruby.h"
 
 void mkd_initialize  		_(( void ));
 void mkd_with_html5_tags	_(( void ));
 
-#if defined(HAVE_RUBY_ENCODING_H) && HAVE_RUBY_ENCODING_H
-#	define M17N_SUPPORTED
-#	include "ruby/encoding.h"
-#endif
 
 /* Replace the macro from encoding.h that refers to static 'rb_encoding_list' */
 #ifdef ENC_FROM_ENCINDEX
